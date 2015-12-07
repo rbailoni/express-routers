8e92ae780bba513437fde3631a6646a326cefac7
Ricardo do Espirito Santo Bailoni <rbailoni@gmail.com>
2015-12-07 17:11:46 -0200

    Implementação dos testes do controller Users

--- test/e2e/users.controller.spec.js
+++ test/e2e/users.controller.spec.js
@@ -16,8 +16,51 @@ describe('Users controller', function () {
 					expect(res.statusCode).to.be.equal(200);
 					expect(res.body).to.be.an('array');
 					expect(res.body)
-						.all.have.property('name')
-						.to.be.an('string');
+						.all.have.property('name');
+					done();
+				});
+		});
+	});
+	describe('.create - POST /users', function () {
+		it('should return a  json {message:\'created\'}', function (done) {
+			request(app)
+				.post('/users')
+				.end(function (err, res) {
+					expect(res.statusCode).to.be.equal(201);
+					expect(res.body).have.property('message','created');
+					done();
+				});
+		});
+	});
+	describe('.get - GET /users/:id', function () {
+		it('should return an user', function (done) {
+			request(app)
+				.get('/users/1234')
+				.end(function (err, res) {
+					expect(res.statusCode).to.be.equal(200);
+					expect(res.body).have.property('name','Ricardo');
+					done();
+				});
+		});
+	});
+	describe('.update - PUT /users/:id', function () {
+		it('should return a json {message:\'updated\'}', function (done) {
+			request(app)
+				.put('/users/1234')
+				.end(function (err, res) {
+					expect(res.statusCode).to.be.equal(200);
+					expect(res.body).have.property('message','updated');
+					done();
+				});
+		});
+	});
+	describe('.delete - DELETE /users/:id', function () {
+		it('should return a json {message:\'deleted\'}', function (done) {
+			request(app)
+				.delete('/users/1234')
+				.end(function (err, res) {
+					expect(res.statusCode).to.be.equal(200);
+					expect(res.body).have.property('message','deleted');
 					done();
 				});
 		});
